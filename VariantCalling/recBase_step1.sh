9643e6239fcd16ee3e1f5b2d92b400b093d831c4
jiaojiaoguo1205 <guo.jiao@husky.neu.edu>
2017-12-07 22:22:45 -0500

    recBase

--- VariantCalling/recBase_step1.sh
+++ VariantCalling/recBase_step1.sh
@@ -5,16 +5,9 @@ replacement="-I noDup/"
 bamParam="${bam//$inPath/$replacement}"
 #echo $bamParam
 knownVcf="filteredSnps.vcf"
-recBase_step1(){
-for bam in $bamPath*.bam
-do
 nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar \
 -T BaseRecalibrator \
 -R vShiloni.fasta \
 $bamParam \
--knownSites $knownVcf \
--o recal_data.table 
-done
-}
-recBase_step1 1>rec.err 2>rec.log & 
+-knownSites $knownVcf -o recal_data.table 1>rec.err 2>rec.log &  
 
--- VariantCalling/recBase_step2.sh
+++ VariantCalling/recBase_step2.sh
@@ -5,17 +5,12 @@ replacement="-I noDup/"
 bamParam="${bam//$inPath/$replacement}"
 #echo $bamParam
 knownVcf="filteredSnps.vcf"
-recBase_step2(){
-for bam in $bamPath*.bam
-do
 nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar \
 -T BaseRecalibrator \
 -R vShiloni.fasta \
 $bamParam \
 -knownSites $knownVcf \
 -BQSR recal_data.table \
--o post_recal_data.table 
-done
-}
-recBase_step2 1>rec2.err 2>rec2.log & 
+-o post_recal_data.table \
+1>rec2.err 2>rec2.log & 
 
